bc77b1263d73367c3a3b05d1688ed9d8cb6e6abb
Ayush Yadav <yadava5@miamioh.edu>
2026-02-06 13:39:58 -0500

    chore(sections): add Experience to barrel export

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -2,8 +2,9 @@
  * @fileoverview Barrel export for section components
  *
  * Re-exports all page section components:
- * import { Hero, About, Projects } from "@/components/sections";
+ * import { Hero, About, Experience, Projects } from "@/components/sections";
  */
 
 export { Hero } from "./Hero";
 export { About } from "./About";
+export { Experience } from "./Experience";
